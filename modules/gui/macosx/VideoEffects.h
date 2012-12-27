8dff4d3b5557e93c3adda163d0f863c89dc4cc9e
David Fuhrmann <david.fuhrmann@googlemail.com>
2012-12-27 16:06:35 +0100

    macosx: video effects: fix text field / stepper syncronisation, fix enabled state of steppers
    (not cherry picked from not yet done commit)

--- modules/gui/macosx/VideoEffects.h
+++ modules/gui/macosx/VideoEffects.h
@@ -77,8 +77,10 @@
     IBOutlet id o_puzzle_ckb;
     IBOutlet id o_puzzle_rows_lbl;
     IBOutlet id o_puzzle_rows_fld;
+    IBOutlet id o_puzzle_rows_stp;
     IBOutlet id o_puzzle_columns_lbl;
     IBOutlet id o_puzzle_columns_fld;
+    IBOutlet id o_puzzle_columns_stp;
     IBOutlet id o_puzzle_blackslot_ckb;
 
     /* color */
@@ -92,6 +94,7 @@
     IBOutlet id o_sepia_ckb;
     IBOutlet id o_sepia_lbl;
     IBOutlet id o_sepia_fld;
+    IBOutlet id o_sepia_stp;
     IBOutlet id o_noise_ckb;
     IBOutlet id o_gradient_ckb;
     IBOutlet id o_gradient_mode_lbl;
@@ -105,6 +108,7 @@
     IBOutlet id o_posterize_ckb;
     IBOutlet id o_posterize_lbl;
     IBOutlet id o_posterize_fld;
+    IBOutlet id o_posterize_stp;
     IBOutlet id o_blur_ckb;
     IBOutlet id o_blur_sld;
     IBOutlet id o_blur_lbl;
