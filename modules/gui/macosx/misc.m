07f809f307688204689932b520ece3f5b0b001dc
Felix Paul Kühne <fkuehne@videolan.org>
2013-03-18 22:40:27 +0100

    macosx: fix time slider knob alignment in bright mode (cherry picked from commit 4caa39f04d760998a057a2d6affbbdad6c6f3c4a)

--- modules/gui/macosx/misc.m
+++ modules/gui/macosx/misc.m
@@ -548,8 +548,6 @@ void _drawFrameInRect(NSRect frameRect)
     NSRect knobRect = [[self cell] knobRectFlipped:NO];
     if (b_dark)
         knobRect.origin.y+=2;
-    else
-        knobRect.origin.y+=1;
     [self drawKnobInRect: knobRect];
 }
 
